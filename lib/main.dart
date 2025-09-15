d5f840f400856e846049d9f771189e0aeb921568
hyun471 <khs101ttl@gmail.com>
2025-09-15 10:36:21 +0900

    🎉 go router, bottom navigation bar 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dailymoji/core/routers/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,10 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
-      home: Scaffold(),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
     );
   }
 }
---
+++ lib/presentation/widgets/bottom_bar.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      color: Colors.grey[200],
+      child: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(40),
+                    onTap: () {
+                      context.push('/next');
+                    },
+                    child: BottomBarIcon(title: 'Home'),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(40),
+                    onTap: () {},
+                    child: BottomBarIcon(title: 'Report'),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(40),
+                    onTap: () {},
+                    child: BottomBarIcon(title: 'My Page'),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//TODO: 바텀에 사용될 아이콘이 정해지면 회색박스 대신 넣어야함
+class BottomBarIcon extends StatelessWidget {
+  final String title;
+  const BottomBarIcon({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              width: 28,
+              height: 28,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(title),
+        ],
+      ),
+    );
+  }
+}
+
+// context.push('/next');
