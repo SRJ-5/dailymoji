c7cdc50c5a9569fff43463afc26528f18c496ee3
hyun471 <khs101ttl@gmail.com>
2025-09-17 14:00:43 +0900

    ✨ google login 기능 1차

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -1,7 +1,34 @@
-import 'package:dailymoji/presentation/widgets/bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+// TODO: test로 여기에 구현 test완료 후 클린아키텍쳐 구조로 변환해야함
+class _LoginPageState extends State<LoginPage> {
+  Future<void> googleLogin() async {
+    // final google = GoogleSignIn();
+    // final result = await google.signIn();
+    // final auth = await result?.authentication;
+    // print('auth: $auth');
+    // print('accessToken: ${auth?.accessToken}');
+    // print('idToken: ${auth?.idToken}');
+    // final response = await Supabase.instance.client.auth
+    //     .signInWithIdToken(
+    //         provider: OAuthProvider.google,
+    //         idToken: auth!.idToken!);
+    // print('user: ${response.user}');
+
+    final response = await Supabase.instance.client.auth
+        .signInWithOAuth(OAuthProvider.google,
+            redirectTo: 'dailymoji://login-callback');
+    final user = Supabase.instance.client.auth.currentUser;
+    print('user: ${user?.email}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,18 +72,28 @@ class LoginPage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Expanded(
-                        child: CircleAvatar(
-                          radius: 30,
-                          child: Image.asset(
-                            'assets/icons/google_login_logo.png',
+                        child: GestureDetector(
+                          onTap: () {
+                            googleLogin();
+                          },
+                          child: CircleAvatar(
+                            radius: 30,
+                            child: Image.asset(
+                              'assets/icons/google_login_logo.png',
+                            ),
                           ),
                         ),
                       ),
                       Expanded(
-                        child: CircleAvatar(
-                          radius: 30,
-                          child: Image.asset(
-                            'assets/icons/apple_login_logo.png',
+                        child: GestureDetector(
+                          onTap: () {
+                            print('애플 로그인 실행');
+                          },
+                          child: CircleAvatar(
+                            radius: 30,
+                            child: Image.asset(
+                              'assets/icons/apple_login_logo.png',
+                            ),
                           ),
                         ),
                       ),
