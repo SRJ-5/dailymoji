685a9c4c4e3636019b9dcd9bce5ec161c5cbcd08
Kihwan-dev <rlrkf2420@gmail.com>
2025-09-20 00:53:53 +0900

    ✨ chat관련 provider 추가

--- lib/presentation/providers.dart
+++ lib/presentation/providers.dart
@@ -1,1 +1,43 @@
+// DataSource Provider
+import 'package:dailymoji/data/data_sources/chat_remote_data_source.dart';
+import 'package:dailymoji/data/data_sources/chat_remote_data_source_impl.dart';
+import 'package:dailymoji/data/repositories/chat_repository_impl.dart';
+import 'package:dailymoji/domain/repositories/chat_repository.dart';
+import 'package:dailymoji/domain/use_cases/load_messages_use_case.dart';
+import 'package:dailymoji/domain/use_cases/send_message_use_case.dart';
+import 'package:dailymoji/domain/use_cases/subscribe_messages_use_case.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final chatRemoteDataSourceProvider = Provider<ChatRemoteDataSource>(
+  (ref) {
+    return ChatRemoteDataSourceImpl();
+  },
+);
+
+final chatRepositoryProvider = Provider<ChatRepository>(
+  (ref) {
+    final dataSource = ref.watch(chatRemoteDataSourceProvider);
+    return ChatRepositoryImpl(dataSource);
+  },
+);
+
+final loadMessagesUseCaseProvider = Provider<LoadMessagesUseCase>(
+  (ref) {
+    final repo = ref.watch(chatRepositoryProvider);
+    return LoadMessagesUseCase(repo);
+  },
+);
+
+final sendMessageUseCaseProvider = Provider<SendMessageUseCase>(
+  (ref) {
+    final repo = ref.watch(chatRepositoryProvider);
+    return SendMessageUseCase(repo);
+  },
+);
+
+final subscribeMessagesUseCaseProvider = Provider<SubscribeMessagesUseCase>(
+  (ref) {
+    final repo = ref.watch(chatRepositoryProvider);
+    return SubscribeMessagesUseCase(repo);
+  },
+);
