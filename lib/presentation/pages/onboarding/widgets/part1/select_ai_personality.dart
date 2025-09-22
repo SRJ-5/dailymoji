4ee6c24c35082d36ab030597126e480f151feb34
hyun471 <khs101ttl@gmail.com>
2025-09-22 12:05:48 +0900

    Feature/onboarding (#13)
    * ✨ .env 추가
    
    * 🚧 온보딩 파트1 UI 구성 중
    
    * ✨ Color 스타일 생성
    ✨ Font 스타일 생성
    ✨ 온보딩 완료
    🚧 페이지 연결 및 서버 연결 구현해야함
    
    * ✨ Color 스타일 생성
    
    ✨ Font 스타일 생성
    
    ✨ 온보딩 완료
    
    🚧 페이지 연결 및 서버 연결 구현해야함
    
    * ♻️ bodyBold가 semiBold로 되어 있던거 수정
    
    * .env 삭제

---
+++ lib/presentation/pages/onboarding/widgets/part1/select_ai_personality.dart
@@ -0,0 +1,92 @@
+import 'package:dailymoji/core/styles/colors.dart';
+import 'package:dailymoji/core/styles/fonts.dart';
+import 'package:dailymoji/presentation/pages/onboarding/view_model/onboarding_view_model.dart';
+import 'package:dailymoji/presentation/pages/onboarding/widgets/select_box.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SelectAiPersonality extends ConsumerStatefulWidget {
+  const SelectAiPersonality({
+    super.key,
+  });
+
+  @override
+  ConsumerState<SelectAiPersonality> createState() =>
+      _SelectAiPersonalityState();
+}
+
+class _SelectAiPersonalityState
+    extends ConsumerState<SelectAiPersonality> {
+  int _selectedIndex = -1;
+
+  final _personalities = [
+    '차분하게 상황을 분석하고 문제를 해결하는 친구',
+    '감정 표현이 풍부하고 따뜻한 친구',
+    '어딘가 엉뚱하지만 마음만은 따뜻한 친구',
+    '따뜻함과 이성적인 사고를 모두 가진 친구'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 24.r,
+        ),
+        Container(
+          width: double.infinity,
+          height: 94.h,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '캐릭터의 성격을\n골라볼까요?',
+              style: AppFontStyles.heading2
+                  .copyWith(color: AppColors.grey900),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 24.r,
+        ),
+        Column(
+          children: List.generate(
+            _personalities.length,
+            (index) {
+              final isSelected = _selectedIndex == index;
+              return Column(
+                children: [
+                  GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedIndex =
+                              (_selectedIndex == index)
+                                  ? -1
+                                  : index;
+                        });
+                        ref
+                            .read(onboardingViewModelProvider
+                                .notifier)
+                            .setAiPersonality(
+                                check: _selectedIndex != -1,
+                                aiPersonality:
+                                    _personalities[index]);
+                      },
+                      child: SelectBox(
+                          isSelected: isSelected,
+                          text: _personalities[index])),
+                  _personalities.length - 1 == index
+                      ? SizedBox.shrink()
+                      : SizedBox(
+                          height: 8.h,
+                        )
+                ],
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/presentation/pages/onboarding/widgets/part1/user_nick_name.dart
@@ -0,0 +1,146 @@
+import 'package:dailymoji/core/styles/colors.dart';
+import 'package:dailymoji/core/styles/fonts.dart';
+import 'package:dailymoji/presentation/pages/onboarding/view_model/onboarding_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class UserNickName extends ConsumerStatefulWidget {
+  const UserNickName({super.key});
+
+  @override
+  ConsumerState<UserNickName> createState() =>
+      _UserNickNameState();
+}
+
+class _UserNickNameState extends ConsumerState<UserNickName> {
+  bool _isNameCheck = true;
+  TextEditingController _textEditingController =
+      TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController(
+        text:
+            ref.read(onboardingViewModelProvider).userNickName);
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _state = ref.read(onboardingViewModelProvider);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 24.r,
+        ),
+        Container(
+          width: double.infinity,
+          height: 94.h,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '${_state.aiName}이(가)\n뭐라고 부르면 될까요?',
+              style: AppFontStyles.heading2,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 24.h,
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: double.infinity,
+            height: 64.h,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TextField(
+                maxLength: 10,
+                controller: _textEditingController,
+                style: AppFontStyles.bodyRegular16
+                    .copyWith(color: AppColors.grey900),
+                onChanged: (value) {
+                  final isValid = value.length >= 2;
+                  setState(() {
+                    if (value.isEmpty) {
+                      _isNameCheck = true;
+                    } else {
+                      _isNameCheck = isValid;
+                    }
+                  });
+                  // TODO: ViewModel로 상태 관리 하여 저장
+                  ref
+                      .watch(
+                          onboardingViewModelProvider.notifier)
+                      .setUserNickName(
+                          check: isValid, userNickName: value);
+                },
+                decoration: InputDecoration(
+                    counterText: '',
+                    hintText: '별명을 입력해 주세요',
+                    hintStyle: AppFontStyles.bodyRegular16
+                        .copyWith(color: AppColors.grey400),
+                    suffixIcon:
+                        _textEditingController.text.isEmpty
+                            ? null
+                            : IconButton(
+                                icon: Icon(Icons.clear),
+                                onPressed: () {
+                                  _textEditingController.clear();
+                                  setState(() {
+                                    _isNameCheck = true;
+                                  });
+                                  ref
+                                      .watch(
+                                          onboardingViewModelProvider
+                                              .notifier)
+                                      .setUserNickName(
+                                          check: false,
+                                          userNickName: '');
+                                },
+                              ),
+                    contentPadding: EdgeInsets.symmetric(
+                        horizontal: 12.w, vertical: 16.h),
+                    filled: true,
+                    fillColor: AppColors.green50,
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1, color: AppColors.grey200),
+                        borderRadius:
+                            BorderRadius.circular(12.r)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1, color: AppColors.green500),
+                        borderRadius:
+                            BorderRadius.circular(12.r)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1, color: AppColors.grey200),
+                        borderRadius:
+                            BorderRadius.circular(12.r))),
+              ),
+            ),
+          ),
+        ),
+        Text('• 2~10자만 사용 가능해요',
+            style: AppFontStyles.bodyRegular12.copyWith(
+                color: _isNameCheck
+                    ? AppColors.grey700
+                    : AppColors.orange500)),
+        Text(
+          '• 나중에 언제든지 변경할 수 있어요',
+          style: AppFontStyles.bodyRegular12
+              .copyWith(color: AppColors.grey700),
+        )
+      ],
+    );
+  }
+}
