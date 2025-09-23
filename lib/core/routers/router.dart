be38dc7900d77de004a9486082593f1886573bdb
Kihwan-dev <rlrkf2420@gmail.com>
2025-09-23 23:55:22 +0900

    ✨ 스플래시 페이지 구현

--- lib/core/routers/router.dart
+++ lib/core/routers/router.dart
@@ -7,6 +7,7 @@ import 'package:dailymoji/presentation/pages/solution/solution_page.dart';
 import 'package:dailymoji/presentation/pages/breathing_solution/breathing_solution_page.dart';
 import 'package:dailymoji/presentation/pages/onboarding/onboarding_part1_page.dart';
 import 'package:dailymoji/presentation/pages/onboarding/onboarding_part2_page.dart';
+import 'package:dailymoji/presentation/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -16,33 +17,20 @@ final router = GoRouter(
   initialLocation: '/',
   navigatorKey: navigatorkey,
   routes: [
-    GoRoute(path: '/', builder: (context, state) => LoginPage()),
-    GoRoute(
-        path: '/onboarding1',
-        builder: (context, state) => OnboardingPart1Page()),
-    GoRoute(
-        path: '/onboarding2',
-        builder: (context, state) => OnboardingPart2Page()),
-    GoRoute(
-        path: '/home',
-        builder: (context, state) => HomePage(),
-        routes: [
-          GoRoute(
-              path: '/ChatPage',
-              builder: (context, state) => ChatPage()),
-        ]),
-    GoRoute(
-        path: '/report',
-        builder: (context, state) => ReportPage()),
+    GoRoute(path: '/', builder: (context, state) => SplashPage()),
+    GoRoute(path: '/login', builder: (context, state) => LoginPage()),
+    GoRoute(path: '/onboarding1', builder: (context, state) => OnboardingPart1Page()),
+    GoRoute(path: '/onboarding2', builder: (context, state) => OnboardingPart2Page()),
+    GoRoute(path: '/home', builder: (context, state) => HomePage(), routes: [
+      GoRoute(path: '/ChatPage', builder: (context, state) => ChatPage()),
+    ]),
+    GoRoute(path: '/report', builder: (context, state) => ReportPage()),
     GoRoute(path: '/my', builder: (context, state) => MyPage()),
-    GoRoute(
-        path: '/BreathingSolutionPage',
-        builder: (context, state) => BreathingSolutionPage(),
-        routes: [
-          GoRoute(
-            path: '/SolutionPage',
-            builder: (context, state) => SolutionPage(),
-          ),
-        ]),
+    GoRoute(path: '/BreathingSolutionPage', builder: (context, state) => BreathingSolutionPage(), routes: [
+      GoRoute(
+        path: '/SolutionPage',
+        builder: (context, state) => SolutionPage(),
+      ),
+    ]),
   ],
 );
---
+++ lib/presentation/pages/splash/splash_page.dart
@@ -0,0 +1,59 @@
+import 'package:dailymoji/presentation/pages/login/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacity;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1), // 페이드아웃 시간 = 1초
+    );
+    _opacity = Tween<double>(begin: 1, end: 0).animate(_controller);
+
+    // 1초 유지 후 → 1초 동안 페이드아웃
+    Future.delayed(Duration(seconds: 2), () {
+      if (mounted) {
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // 뒤에 LoginPage
+        LoginPage(),
+
+        // 위에 SplashPage 이미지 (페이드아웃)
+        FadeTransition(
+          opacity: _opacity,
+          child: SizedBox.expand(
+            child: Image.asset(
+              "assets/images/splash_image.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
