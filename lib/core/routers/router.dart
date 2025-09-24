45217941dabb9f5c0d715746e3ff6a606f1ed6c0
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-09-25 01:05:59 +0900

    Feature/add-character-setting-page (#28)
    * ✨ 캐릭터 성격 이넘 추가
    
    * ✨ svg 패미지 및 asset 추가
    
    * ✨ 캐릭터 세팅 페이지 추가 및 관련 기능 구현
    
    * ♻️ enum 데이터 위치 변경
    
    * 💄 스플래시 이미지 변경
    
    * ♻️ 이넘 적용
    
    * ♻️ 닉네임 최소 2글자로 변경

--- lib/core/routers/router.dart
+++ lib/core/routers/router.dart
@@ -1,3 +1,4 @@
+import 'package:dailymoji/presentation/pages/character_setting/character_setting_page.dart';
 import 'package:dailymoji/presentation/pages/chat/chat_page.dart';
 import 'package:dailymoji/presentation/pages/home/home_page.dart';
 import 'package:dailymoji/presentation/pages/info/info_page.dart';
@@ -18,45 +19,27 @@ final router = GoRouter(
   initialLocation: '/',
   navigatorKey: navigatorkey,
   routes: [
+    GoRoute(path: '/', builder: (context, state) => SplashPage()),
+    GoRoute(path: '/login', builder: (context, state) => LoginPage()),
+    GoRoute(path: '/onboarding1', builder: (context, state) => OnboardingPart1Page()),
+    GoRoute(path: '/onboarding2', builder: (context, state) => OnboardingPart2Page()),
+    GoRoute(path: '/home', builder: (context, state) => HomePage(), routes: [
+      GoRoute(path: '/ChatPage', builder: (context, state) => ChatPage()),
+    ]),
+    GoRoute(path: '/report', builder: (context, state) => ReportPage()),
     GoRoute(
-        path: '/', builder: (context, state) => SplashPage()),
-    GoRoute(
-        path: '/login',
-        builder: (context, state) => LoginPage()),
-    GoRoute(
-        path: '/onboarding1',
-        builder: (context, state) => OnboardingPart1Page()),
-    GoRoute(
-        path: '/onboarding2',
-        builder: (context, state) => OnboardingPart2Page()),
-    GoRoute(
-        path: '/home',
-        builder: (context, state) => HomePage(),
-        routes: [
-          GoRoute(
-              path: '/ChatPage',
-              builder: (context, state) => ChatPage()),
-        ]),
-    GoRoute(
-        path: '/report',
-        builder: (context, state) => ReportPage()),
-    GoRoute(
-        path: '/my',
-        builder: (context, state) => MyPage(),
-        routes: [
-          GoRoute(
-            path: 'info',
-            builder: (context, state) => InfoPage(),
-          )
-        ]),
-    GoRoute(
-        path: '/BreathingSolutionPage',
-        builder: (context, state) => BreathingSolutionPage(),
-        routes: [
-          GoRoute(
-            path: '/SolutionPage',
-            builder: (context, state) => SolutionPage(),
-          ),
-        ]),
+      path: '/my',
+      builder: (context, state) => MyPage(),
+      routes: [
+        GoRoute(path: 'info', builder: (context, state) => InfoPage()),
+        GoRoute(path: 'characterSetting', builder: (context, state) => CharacterSettingPage()),
+      ],
+    ),
+    GoRoute(path: '/BreathingSolutionPage', builder: (context, state) => BreathingSolutionPage(), routes: [
+      GoRoute(
+        path: '/SolutionPage',
+        builder: (context, state) => SolutionPage(),
+      ),
+    ]),
   ],
 );
---
+++ lib/domain/enums/enum_data.dart
@@ -0,0 +1,14 @@
+enum Sender { user, bot }
+
+enum MessageType { normal, solution }
+
+enum CharacterPersonality {
+  probSolver("문제 해결을 잘함", "prob_solver"),
+  warmHeart("감정 풍부하고 따뜻함", "warm_heart"),
+  oddKind("엉뚱하지만 따뜻함", "odd_kind"),
+  balanced("따뜻함과 이성 모두 가짐", "balanced");
+
+  final String label;
+  final String dbValue;
+  const CharacterPersonality(this.label, this.dbValue);
+}
