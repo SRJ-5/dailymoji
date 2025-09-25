84333b17df55c3e68f33a8420dba51ac5cb96ad9
So2ln <sorrinj77@snu.ac.kr>
2025-09-26 01:04:39 +0900

    🐛 로그인페이지 깃에서 복붙

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -75,38 +75,6 @@ class _LoginPageState extends ConsumerState<LoginPage> {
     }
   }
 
-  //
-  // Rin: 가입여부 확인하고 프로필 이미 있으면 넘어가는 함수 따로 뺌
-  Future<void> _handleLogin(Future<String?> loginFuture) async {
-    try {
-      final userId = await loginFuture;
-      if (userId != null && mounted) {
-        // 로그인 성공 후, 프로필이 있는지 확인
-        final isRegistered = await ref
-            .read(userViewModelProvider.notifier)
-            .getUserProfile(userId);
-        if (mounted) {
-          // 프로필 유무에 따라 다른 페이지로 이동
-          if (isRegistered) {
-            context.go('/home'); // 이미 가입했으면 홈으로
-          } else {
-            context.go('/onboarding1'); // 처음이면 온보딩으로
-          }
-        }
-      } else if (mounted) {
-        // TODO: 로그인 실패 처리 디자인하기!!
-        ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text("로그인에 실패했습니다. 다시 시도해주세요.")));
-      }
-    } catch (e) {
-      // 예외 처리
-      if (mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text("오류가 발생했습니다: ${e.toString()}")));
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final platform = Theme.of(context).platform;
@@ -117,27 +85,28 @@ class _LoginPageState extends ConsumerState<LoginPage> {
       ),
       body: Column(
         children: [
-          Spacer(),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 47.44.h,
-                  child: Image.asset(
-                    'assets/icons/dailymoji_logo.png',
-                    fit: BoxFit.cover,
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 47.44.h,
+                    child: Image.asset(
+                      'assets/icons/dailymoji_logo.png',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 8.h,
-                ),
-                Text(
-                  '매일매일 감정 관리',
-                  style: AppFontStyles.bodyRegular18
-                      .copyWith(color: AppColors.grey500),
-                ),
-              ],
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  Text(
+                    '매일매일 감정 관리',
+                    style: AppFontStyles.bodyRegular18
+                        .copyWith(color: AppColors.grey500),
+                  ),
+                ],
+              ),
             ),
           ),
           Container(
