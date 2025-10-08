75a217bfbc1d3cb4bcc39c85c8326ed39fd65cc0
hyun471 <khs101ttl@gmail.com>
2025-10-09 01:47:27 +0900

    🐛 로그아웃 팝업 버튼 및 텍스트 색상 수정

--- lib/presentation/pages/my/widgets/confirm_dialog.dart
+++ lib/presentation/pages/my/widgets/confirm_dialog.dart
@@ -55,12 +55,14 @@ class ConfirmDialog extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userProfileVM = ref.read(userViewModelProvider.notifier);
+    final userProfileVM =
+        ref.read(userViewModelProvider.notifier);
     // 배경 터치는 닫기, 다이얼로그 영역은 차단
     return GestureDetector(
       onTap: () => context.pop(), // 배경 터치시 다이얼로그 닫기
       child: Material(
-        color: AppColors.black.withValues(alpha: 0.35), // 배경 오버레이
+        color:
+            AppColors.black.withValues(alpha: 0.35), // 배경 오버레이
         child: Center(
           child: GestureDetector(
             // 다이얼로그 컨테이너 터치시 이벤트 차단 (배경 터치 이벤트가 전파되지 않음)
@@ -78,7 +80,9 @@ class ConfirmDialog extends ConsumerWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   AppText(
-                    isDeleteAccount ? "정말 탈퇴하시겠어요?" : "로그아웃 하시겠어요?",
+                    isDeleteAccount
+                        ? "정말 탈퇴하시겠어요?"
+                        : "로그아웃 하시겠어요?",
                     style: AppFontStyles.heading3
                         .copyWith(color: AppColors.grey900),
                   ),
@@ -95,15 +99,18 @@ class ConfirmDialog extends ConsumerWidget {
                           height: 48.h,
                           decoration: BoxDecoration(
                             color: AppColors.green50,
-                            borderRadius: BorderRadius.circular(12.r),
-                            border:
-                                Border.all(width: 1, color: AppColors.grey200),
+                            borderRadius:
+                                BorderRadius.circular(12.r),
+                            border: Border.all(
+                                width: 1,
+                                color: AppColors.grey200),
                           ),
                           child: Center(
                             child: AppText(
                               '취소',
                               style: AppFontStyles.bodyMedium16
-                                  .copyWith(color: AppColors.grey900),
+                                  .copyWith(
+                                      color: AppColors.grey900),
                             ),
                           ),
                         ),
@@ -130,16 +137,16 @@ class ConfirmDialog extends ConsumerWidget {
                           decoration: BoxDecoration(
                             color: isDeleteAccount
                                 ? AppColors.noti900
-                                : AppColors.green400,
-                            borderRadius: BorderRadius.circular(12.r),
+                                : AppColors.green500,
+                            borderRadius:
+                                BorderRadius.circular(12.r),
                           ),
                           child: Center(
                             child: AppText(
                               '확인',
-                              style: AppFontStyles.bodyMedium16.copyWith(
-                                  color: isDeleteAccount
-                                      ? AppColors.grey50
-                                      : AppColors.grey900),
+                              style: AppFontStyles.bodyMedium16
+                                  .copyWith(
+                                      color: AppColors.grey50),
                             ),
                           ),
                         ),
