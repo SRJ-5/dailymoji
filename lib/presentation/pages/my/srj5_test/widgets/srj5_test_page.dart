c73dafb182657cce742ad1a54d3dc1e71ebbc50f
So2ln <sorrinj77@snu.ac.kr>
2025-10-13 06:40:46 +0900

    RIN 추가!

--- lib/presentation/pages/my/srj5_test/widgets/srj5_test_page.dart
+++ lib/presentation/pages/my/srj5_test/widgets/srj5_test_page.dart
@@ -15,8 +15,7 @@ class Srj5TestPage extends ConsumerStatefulWidget {
   Srj5TestPage({super.key});
 
   @override
-  ConsumerState<Srj5TestPage> createState() =>
-      _Srj5TestPageState();
+  ConsumerState<Srj5TestPage> createState() => _Srj5TestPageState();
 }
 
 class _Srj5TestPageState extends ConsumerState<Srj5TestPage> {
@@ -31,9 +30,7 @@ class _Srj5TestPageState extends ConsumerState<Srj5TestPage> {
     final questionList = clusterState.questionText;
     totalSteps = questionList.length;
     final isNextEnabled =
-        testState.questionScores?[stepIndex] == -1
-            ? false
-            : true;
+        testState.questionScores?[stepIndex] == -1 ? false : true;
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.yellow50,
@@ -51,8 +48,8 @@ class _Srj5TestPageState extends ConsumerState<Srj5TestPage> {
             ? null
             : AppText(
                 '${clusterState.clusterNM!} 감정 검사',
-                style: AppFontStyles.bodyBold18
-                    .copyWith(color: AppColors.grey900),
+                style:
+                    AppFontStyles.bodyBold18.copyWith(color: AppColors.grey900),
               ),
         centerTitle: true,
         actions: [
@@ -81,8 +78,7 @@ class _Srj5TestPageState extends ConsumerState<Srj5TestPage> {
             Expanded(
                 child: stepIndex == totalSteps
                     ? FinishWidget(
-                        text:
-                            '좋아요!\n${user.userNickNm}님을 좀 더 이해하게 됐어요',
+                        text: '좋아요!\n${user.userNickNm}님을 좀 더 이해하게 됐어요',
                         srj5: true,
                       )
                     : Srj5TestBox(
@@ -104,9 +100,8 @@ class _Srj5TestPageState extends ConsumerState<Srj5TestPage> {
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     minimumSize: Size(double.infinity, 52.h),
-                    backgroundColor: isNextEnabled
-                        ? AppColors.green500
-                        : AppColors.grey200,
+                    backgroundColor:
+                        isNextEnabled ? AppColors.green500 : AppColors.grey200,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12.r),
                     ),
@@ -119,14 +114,17 @@ class _Srj5TestPageState extends ConsumerState<Srj5TestPage> {
                             });
                           } else if (stepIndex == totalSteps) {
                             // ViewModel의 state를 직접 넘기지 않고, ViewModel 내부 함수를 호출
-                            // ref
-                            //     .read(assessmentViewModelProvider.notifier).;
+                            //TODO: RIN: supabase 저장 로직이 없는거같아서??
+                            ref
+                                .read(assessmentViewModelProvider.notifier)
+                                .submitAssessment(
+                                    user.id!, clusterState.clusterNM!);
+
                             context.go('/home');
                           }
                         }
                       : null,
-                  child: AppText(
-                      stepIndex == totalSteps ? '완료' : '다음으로',
+                  child: AppText(stepIndex == totalSteps ? '완료' : '다음으로',
                       style: AppFontStyles.bodyMedium16.copyWith(
                         color: isNextEnabled
                             ? AppColors.grey50
