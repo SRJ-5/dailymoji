41306e524500272c28da4f9e672f53a06bb133b4
So2ln <sorrinj77@snu.ac.kr>
2025-10-15 19:14:40 +0900

    ♻️ notification_service 함수를 user profile data source impl에 옮김

--- lib/data/data_sources/user_profile_data_source_impl.dart
+++ lib/data/data_sources/user_profile_data_source_impl.dart
@@ -172,30 +172,18 @@ class UserProfileDataSourceImpl implements UserProfileDataSource {
         print("⚠️ 유저가 로그인되지 않았습니다. FCM 토큰 저장 스킵");
         return;
       }
-      late String token;
-      if (platform == TargetPlatform.iOS) {
-        final iosToken = await FirebaseMessaging.instance.getToken();
-        if (iosToken == null) {
-          print("⚠️ FCM ios 토큰을 가져올 수 없습니다.");
-          return;
-        }
-        token = iosToken;
-      } else if (platform == TargetPlatform.android) {
-        final androidToken = await FirebaseMessaging.instance.getToken();
-        if (androidToken == null) {
-          print("⚠️ FCM android 토큰을 가져올 수 없습니다.");
-          return;
-        }
-        token = androidToken;
-      }
 
-      print("❤️ FCM tocken!! : ${token}");
+      final token = await FirebaseMessaging.instance.getToken();
+      if (token == null) {
+        print("⚠️ FCM 토큰을 가져올 수 없습니다.");
+        return;
+      }
 
       await supabase.from('user_tokens').upsert({
         'user_id': user.id,
         'token': token,
         'updated_at': DateTime.now().toIso8601String(),
-      });
+      }, onConflict: 'user_id');
 
       print("✅ Supabase에 FCM 토큰 저장 완료: $token");
     } catch (e) {
