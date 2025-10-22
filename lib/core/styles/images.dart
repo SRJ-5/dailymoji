01de0e344f6694dae5c286aea69126a462843e56
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-10-22 11:15:16 +0900

    Fix/third-QA-feedback (#119)
    * 🐛 건강 면책 고지 사항 최대 높이 해제
    
    * 🐛 채팅 인풋 필드 터치 영역 확장
    
    * 🐛 솔루션 메세지 최대 너비 고정되도록 수정
    
    * 🐛 이모지바 평온/회복 잘림 수정
    
    * 🐛 채팅 인풋필드 여백 수정
    
    * 🐛 .이모지바 오류 수정
    
    * 🐛 채티 인풋 필드 우측 패딩 추가
    
    * 🔥 images.dart 중복 내용 제거

--- lib/core/styles/images.dart
+++ lib/core/styles/images.dart
@@ -1,104 +1,61 @@
 class AppImages {
   // images
   static const String homeImage = 'assets/images/home_image.png';
-  static const String splashImage =
-      'assets/images/splash_image.png';
-  static const String preparingImage =
-      'assets/images/preparing_image.png';
-  static const String srj5TestStart =
-      'assets/images/srj5_test_start.png';
-  static const String srj5TestFinish =
-      'assets/images/srj5_test_finish.png';
-  static const String montlyTutorial =
-      'assets/images/monthly_tutorial.png';
-  static const String weeklyTutorial =
-      'assets/images/weekly_tutorial.png';
+  static const String splashImage = 'assets/images/splash_image.png';
+  static const String preparingImage = 'assets/images/preparing_image.png';
+  static const String srj5TestStart = 'assets/images/srj5_test_start.png';
+  static const String srj5TestFinish = 'assets/images/srj5_test_finish.png';
 
   // images - logo
   // static const String dailymojiLogoBlack =
   //     'assets/images/logo/dailymoji_logo_black.png';
   // static const String dailymojiLogoColor =
   //     'assets/images/logo/dailymoji_logo_color.png';
-  static const String appleLoginLogo =
-      'assets/images/logo/apple_login_logo.png';
-  static const String googleLoginLogo =
-      'assets/images/logo/google_login_logo.png';
+  static const String appleLoginLogo = 'assets/images/logo/apple_login_logo.png';
+  static const String googleLoginLogo = 'assets/images/logo/google_login_logo.png';
 
   // images - emojis
-  static const String angryEmoji =
-      'assets/images/emojis/angry.png';
-  static const String cryingEmoji =
-      'assets/images/emojis/crying.png';
-  static const String shockedEmoji =
-      'assets/images/emojis/shocked.png';
-  static const String sleepingEmoji =
-      'assets/images/emojis/sleeping.png';
-  static const String smileEmoji =
-      'assets/images/emojis/smile.png';
-  static const String defaultEmoji =
-      'assets/images/emojis/default.png';
+  static const String angryEmoji = 'assets/images/emojis/angry.png';
+  static const String cryingEmoji = 'assets/images/emojis/crying.png';
+  static const String shockedEmoji = 'assets/images/emojis/shocked.png';
+  static const String sleepingEmoji = 'assets/images/emojis/sleeping.png';
+  static const String smileEmoji = 'assets/images/emojis/smile.png';
+  static const String defaultEmoji = 'assets/images/emojis/default.png';
 
   // images - back_ground
-  static const String bgHouse =
-      'assets/images/back_ground/bg_house.png';
-  static const String bgRain =
-      'assets/images/back_ground/bg_rain.png';
-  static const String bgSnow =
-      'assets/images/back_ground/bg_snow.png';
-  static const String bgForest =
-      'assets/images/back_ground/bg_forest.png';
+  static const String bgHouse = 'assets/images/back_ground/bg_house.png';
+  static const String bgRain = 'assets/images/back_ground/bg_rain.png';
+  static const String bgSnow = 'assets/images/back_ground/bg_snow.png';
+  static const String bgForest = 'assets/images/back_ground/bg_forest.png';
 
   // images - character_cado
-  static const String cadoFace =
-      'assets/images/character_cado/cado_face.png';
-  static const String cadoLove =
-      'assets/images/character_cado/cado_love.png';
-  static const String cadoProfile =
-      'assets/images/character_cado/cado_profile.png';
-  static const String cadoWalk =
-      'assets/images/character_cado/cado_walk.png';
-  static const String cadoTest =
-      'assets/images/character_cado/cado_test.png';
-  static const String cadoCalender =
-      'assets/images/character_cado/cado_calender.png';
-  static const String cadoRecord =
-      'assets/images/character_cado/cado_record.png';
-  static const String cadoLookUp =
-      'assets/images/character_cado/cado_look_up.png';
-  static const String cadoSideView =
-      'assets/images/character_cado/cado_side_view.png';
+  static const String cadoFace = 'assets/images/character_cado/cado_face.png';
+  static const String cadoLove = 'assets/images/character_cado/cado_love.png';
+  static const String cadoProfile = 'assets/images/character_cado/cado_profile.png';
+  static const String cadoWalk = 'assets/images/character_cado/cado_walk.png';
+  static const String cadoTest = 'assets/images/character_cado/cado_test.png';
+  static const String cadoCalender = 'assets/images/character_cado/cado_calender.png';
+  static const String cadoRecord = 'assets/images/character_cado/cado_record.png';
+  static const String cadoLookUp = 'assets/images/character_cado/cado_look_up.png';
+  static const String cadoSideView = 'assets/images/character_cado/cado_side_view.png';
 
   // images - character_carrot
-  static const String carrotChat =
-      'assets/images/character_carrot/carrot_chat.png';
-  static const String carrotLove =
-      'assets/images/character_carrot/carrot_love.png';
-  static const String carrotProfile =
-      'assets/images/character_carrot/carrot_profile.png';
-  static const String carrotFace =
-      'assets/images/character_carrot/carrot_face.png';
-  static const String carrotWalk =
-      'assets/images/character_carrot/carrot_walk.png';
-  static const String carrotLeftWalk =
-      'assets/images/character_carrot/carrot_left_walk.png';
-  static const String carrotFrontView =
-      'assets/images/character_carrot/carrot_front_view.png';
-  static const String carrotLookUp =
-      'assets/images/character_carrot/carrot_look_up.png';
+  static const String carrotChat = 'assets/images/character_carrot/carrot_chat.png';
+  static const String carrotLove = 'assets/images/character_carrot/carrot_love.png';
+  static const String carrotProfile = 'assets/images/character_carrot/carrot_profile.png';
+  static const String carrotFace = 'assets/images/character_carrot/carrot_face.png';
+  static const String carrotWalk = 'assets/images/character_carrot/carrot_walk.png';
+  static const String carrotLeftWalk = 'assets/images/character_carrot/carrot_left_walk.png';
+  static const String carrotFrontView = 'assets/images/character_carrot/carrot_front_view.png';
+  static const String carrotLookUp = 'assets/images/character_carrot/carrot_look_up.png';
 
   //tutorial
-  static const String montlyTutorial =
-      'assets/images/tutorial/monthly_tutorial.png';
-  static const String weeklyTutorial =
-      'assets/images/tutorial/weekly_tutorial.png';
-  static const String homeTutorial =
-      'assets/images/tutorial/home_tutorial.png';
+  static const String montlyTutorial = 'assets/images/tutorial/monthly_tutorial.png';
+  static const String weeklyTutorial = 'assets/images/tutorial/weekly_tutorial.png';
+  static const String homeTutorial = 'assets/images/tutorial/home_tutorial.png';
 
   // images - characterList
-  static const List<String> characterListProfile = [
-    AppImages.cadoProfile,
-    AppImages.carrotProfile
-  ];
+  static const List<String> characterListProfile = [AppImages.cadoProfile, AppImages.carrotProfile];
   static const List<String> characterListLove = [
     AppImages.cadoLove,
     AppImages.carrotLove,
