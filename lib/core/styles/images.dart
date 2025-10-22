7e8163e0eca238612df589b63e9421fb204607f6
hyun471 <khs101ttl@gmail.com>
2025-10-22 10:36:36 +0900

    🔥 month, week 튜토리얼 style에 두개 있는거 하나씩 삭제

--- lib/core/styles/images.dart
+++ lib/core/styles/images.dart
@@ -9,10 +9,6 @@ class AppImages {
       'assets/images/srj5_test_start.png';
   static const String srj5TestFinish =
       'assets/images/srj5_test_finish.png';
-  static const String montlyTutorial =
-      'assets/images/monthly_tutorial.png';
-  static const String weeklyTutorial =
-      'assets/images/weekly_tutorial.png';
 
   // images - logo
   // static const String dailymojiLogoBlack =
